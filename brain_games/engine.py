a722a71d2be38ec353f93cde7fe98c7c56e2addd
Attrolo <yurin-d@mail.ru>
2023-01-15 15:36:12 +0300

    update engine.py

---
+++ brain_games/engine.py
@@ -0,0 +1,29 @@
+import prompt
+
+from brain_games.game.even_game import get_game_data
+
+
+GAME_ROUND = 3
+
+
+def main(game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    counter = 0
+    while counter < GAME_ROUND:
+        question, correct_answer = get_game_data()
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if answer != str(correct_answer):
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'."
+                  f"\nLet's try again, {name}!")
+            break
+        elif counter == GAME_ROUND - 1:
+            print(f'Congratulations, {name}!')
+            break
+        else:
+            print('Correct!')
+            counter += 1
