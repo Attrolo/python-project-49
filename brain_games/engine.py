bb16f3d6318df72b5deb13dd7e9d9c4581915625
Attrolo <yurin-d@mail.ru>
2023-01-15 15:11:43 +0300

    update files

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,9 +8,11 @@ def main(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    counter = 0
-    while counter < ROUNDS:
+    pritn('Answer "yes" if the number is even, otherwise answer "no".')
+    print(announcement)
+    count = 0
+    while count < ROUNDS:
+        question, correct_answer = game.get_game_data()
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if answer != str(correct_answer):
@@ -18,9 +20,9 @@ def main(game):
                   f"Correct answer was '{correct_answer}'."
                   f"\nLet's try again, {name}!")
             break
-        elif counter == ROUNDS - 1:
+        elif count == ROUNDS - 1:
             print(f'Congratulations, {name}!')
             break
         else:
             print('Correct!')
-            counter += 1
+            count += 1
