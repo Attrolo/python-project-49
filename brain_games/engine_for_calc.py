cfd3b7de6fec6d1e5d11ef45cd068e9fc4b81e02
Attrolo <yurin-d@mail.ru>
2023-01-16 22:37:58 +0300

    update files

--- brain_games/engine_for_calc.py
+++ brain_games/engine_for_calc.py
@@ -1,11 +1,19 @@
 import prompt
 
 
-ROUNDS = 3
-
-
-def main(game):
+def start_game(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print('What is the result of the expression?')
+    print(game.TASK)
+    for i in range(3):
+        question, correct_answer = game.get_question_correct_answer()
+        print(question)
+        answer = prompt.string('Your answer: ')
+        if answer != correct_answer:
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was '\
+{correct_answer}'. Let's try again, {name}!")
+        else:
+            print('Correct!')
+            i += 1
+    print(f'Congratulations, {name}!')
