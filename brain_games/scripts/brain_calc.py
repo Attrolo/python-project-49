4e522ec870485040e394f6fccc9f2fc887238fd1
Attrolo <yurin-d@mail.ru>
2023-01-16 22:03:39 +0300

    update files

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from brain_games.game import calc_game
 
-# тут должен быть запуск игры
+def main():
+    brain_games.engine.main(calc_game)
 
 if __name__ == '__main__':
     main()
