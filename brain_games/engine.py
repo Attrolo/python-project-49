73a9cc2fa1f297c2b3723f8066dbd0afd95a2bec
Attrolo <yurin-d@mail.ru>
2023-01-17 18:34:39 +0300

    update file

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,7 +8,8 @@ def main(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    # print('Answer "yes" if the number is even, otherwise answer "no".') Заменить чтобы сообщение соответствовало логике каждой игры
+    # print('Answer "yes" if the number is even, otherwise answer "no".') ///
+    # Заменить чтобы сообщение соответствовало логике каждой игры
     message_game = game.MESSAGE_GAME
     print(message_game)
     counter = 0
