981767aaec8da736558142c24dadf19e1829e8ec
Attrolo <yurin-d@mail.ru>
2023-01-19 18:42:26 +0300

    update file

--- brain_games/game/gcd_game.py
+++ brain_games/game/gcd_game.py
@@ -4,3 +4,17 @@ from random import randint
 MESSAGE_GAME = 'Find the greatest common divisor of given numbers.'
 
 
+def get_nod(num1, num2):
+    num = 0
+    for i in range(1, min(num1, num2) + 1):
+        if num2 % i == 0 and num1 % i == 0:
+            num = max(num, i)
+    return num
+
+
+def get_game():
+    num1 = randint(1, 10)
+    num2 = randint(1, 10)
+    result = get_nod(num1, num2)
+    question = f"question: {num1} {num2}"
+    return question, result
