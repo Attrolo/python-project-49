a19a4653f6854c6a97895102ef2c0f2f8f8d500d
Attrolo <yurin-d@mail.ru>
2023-01-19 18:28:27 +0300

    update gcd_game

--- brain_games/game/gcd_game.py
+++ brain_games/game/gcd_game.py
@@ -4,17 +4,3 @@ from random import randint
 MESSAGE_GAME = 'Find the greatest common divisor of given numbers.'
 
 
-def get_nod(a, b):
-    while b > 0:
-        c = a % b
-        a = b
-        b = c
-    else:
-        b = 1
-    print(a)
-
-
-def get_game():
-    question = randint(1, 10)
-    correct_answer = get_nod(question)
-    return question, correct_answer
