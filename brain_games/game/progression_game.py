e22dc7cf97eed88b6e753aec319a2117e2aaa5c5
Attrolo <yurin-d@mail.ru>
2023-01-19 19:54:38 +0300

    add new file and update pyproject.toml

---
+++ brain_games/game/progression_game.py
@@ -0,0 +1,10 @@
+from random import randint
+
+
+MESSAGE_GAME = 'What number is missing in the progression?'
+
+
+# Тут будет функция
+
+
+def get_game():
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.game import progression_game
+import brain_games.engine
+
+
+def main():
+    brain_games.engine.main(progression_game)
+
+
+if __name__ == '__main__':
+    main()
