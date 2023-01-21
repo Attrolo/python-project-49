2bb9ec2684c9c84a8dee47aa160f6605c8e86eab
Attrolo <yurin-d@mail.ru>
2023-01-21 19:18:49 +0300

    update file

--- brain_games/game/progression_game.py
+++ brain_games/game/progression_game.py
@@ -1,18 +1,28 @@
 from random import randint
+from random import choice
 
 
 MESSAGE_GAME = 'What number is missing in the progression?'
 
 
 def get_progression():
-    step = randint(1, 6)
-    lenght_progression = randint(5, 21)
-    number = randint(0, 50)  # Почему не используется?
-    for i in range(1, lenght_progression):
-        progression = []  # Создаем пустой список
-        progression.append(progression + step)  # Добавляю к списку элементы
-        to_hide = randint(0, 6)  # Прячем элемент из списка
-        correct_answer = to_hide
-        progression_2 = ''.join(str, progression)
-        question = progression_2.replace(str(correct_answer)), '...'
-        return correct_answer, question
+    step = randint(2, 5)
+    lenght_progression = randint(5, 10)  # Минимальное значение по условию = 5
+    first_number = randint(0, 50)
+    progression = []
+    progression.append(first_number)
+    i = 0
+    while i < lenght_progression:
+        first_number += step
+        progression.append(first_number)
+        i += 1
+    return progression
+
+
+def get_game():
+    question = get_progression()
+    correct_answer = choice(question)
+    index = question.index(correct_answer)
+    question[index] = '..'
+    question = ' '.join(str(element) for element in question)
+    return question, correct_answer
