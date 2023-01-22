d921b10604bb6db3cc9ef5b3b87934ac4b131cd0
Attrolo <yurin-d@mail.ru>
2023-01-22 16:35:50 +0300

    update files

---
+++ brain_games/game/prime_game.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+GAME_MESSAGE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number):
+    divider = 2
+    while number % divider != 0:
+        divider += 1
+    return divider == number
+
+
+def get_game():
+    number = randint(1, 50)
+    if is_prime(number):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    question = f'Question: {number}'
+    return question, correct_answer
