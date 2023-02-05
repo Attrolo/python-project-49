e8ae67151ec1fc38c864e7f21c6cf6ead8ed9de7
Attrolo <yurin-d@mail.ru>
2023-02-05 16:57:56 +0300

    update file

---
+++ brain_games/scripts/brain_calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.game import calculator_game
+import brain_games.engine
+
+
+def main():
+    brain_games.engine.main(calculator_game)
+
+
+if __name__ == '__main__':
+    main()
